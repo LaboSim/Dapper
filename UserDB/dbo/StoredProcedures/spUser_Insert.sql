0b84046ac017608448eedb6c8e2e39df042dfeb7
Szymon <45732684+LaboSim@users.noreply.github.com>
2022-05-29 16:40:48 +0200

    Added spUser_Update procedure

--- UserDB/dbo/StoredProcedures/spUser_Insert.sql
+++ UserDB/dbo/StoredProcedures/spUser_Insert.sql
@@ -1,8 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[spUser_Insert]
-	@FirtsName NVARCHAR(50),
+	@FirstName NVARCHAR(50),
 	@LastName NVARCHAR(50)
 AS
 BEGIN
 	INSERT INTO [dbo].[User] (FirstName, LastName)
-	VALUES (@FirtsName, @LastName)
+	VALUES (@FirstName, @LastName)
 END
