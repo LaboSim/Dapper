dda19f955337c5cfff99107a8daac83fe9f4b964
Szymon <45732684+LaboSim@users.noreply.github.com>
2022-05-30 20:25:45 +0200

    Handling data access

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data
+{
+    public class UserData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UserData(ISqlDataAccess db)
+        {
+            this._db = db;
+        }
+
+        public Task<IEnumerable<UserModel>> GetUsers()
+        {
+            return _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new {  });
+        }
+
+        public async Task<UserModel?> GetUser(int id)
+        {
+            var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new { Id = id });
+            return results.FirstOrDefault();
+        }
+    }
+}
